c7fba506c86109eac8aee248659622c91f012459
viniciuskj <vinicius.koe@puccampinas.edu.br>
2025-05-14 14:35:42 -0300

    fix: add handlerError

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
 import fastify from 'fastify'
 import { appRoutes } from './routes/routes'
+import { ZodError } from 'zod'
+import { env } from 'env'
 
 export const app = fastify()
 
 app.register(appRoutes)
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation Error', issues: error.format() })
+  }
+  if (env.NODE_ENV !== 'prod') {
+    console.error(error)
+  } else {
+    // Deve fazer um log pra um tratamento externo
+  }
+
+  return reply.status(500).send({ message: 'Internal server error' })
+})
--- src/controllers/resgisterController.ts
+++ src/controllers/resgisterController.ts
@@ -29,7 +29,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
         message: error.message,
       })
     }
-    return reply.status(500)
+    throw error
   }
 
   return reply.status(201).send()

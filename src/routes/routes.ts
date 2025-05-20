465e89e944531e99499c001d8031af17bd053e38
viniciuskj <vinicius.koe@puccampinas.edu.br>
2025-05-20 12:29:44 -0300

    feat: jwt implementation

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,12 @@
 import { authenticate } from '@/controllers/authenticateController'
 import { register } from '@/controllers/resgisterController'
+import { profile } from '@/controllers/profileController'
 import { FastifyInstance } from 'fastify'
 
 // Nao se usa get quando puxa body, apenas post
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.get('/profile', profile)
 }
